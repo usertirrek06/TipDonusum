a48767047f48fa07b54870c97ea8321a82897cfd
usertirrek06 <71853456+usertirrek06@users.noreply.github.com>
2022-08-04 23:59:58 +0300

    Initial Commit

---
+++ TipDonusumleri/TipDonusumleri/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TipDonusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Conversion
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+            Console.WriteLine(d);
+
+            long h = d;
+            Console.WriteLine(h);
+
+            float f = h;
+            Console.WriteLine(f);
+
+            string name = "Tarik";
+            char n = 'c';
+            object g = name + n;
+            Console.WriteLine(g);
+
+            //Explicit Conversion
+            int z = 23;
+            byte y = (byte)z;
+            Console.WriteLine(y);
+            var yy = Convert.ToString(y);
+            Console.WriteLine(yy);
+
+        }
+    }
+}
